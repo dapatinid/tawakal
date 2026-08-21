b2dce1e48791aec55e7e3cf11fd6180c01a541b4
Dapatin ID <dapatin.id@gmail.com>
2026-08-21 14:31:42 +0700

    Ganti Logo

---
+++ public/build/assets/useFormControl-DF6P1G0S.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-DRVI6WZg.js";import{A as t,S as e}from"./app-BPGazn1E.js";function n(o){return t(()=>e(o)?!!r(o)?.closest("form"):!0)}export{n as u};
--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -93,7 +93,7 @@ onUnmounted(() => {
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Dapatin.id</span
+            >UD.Tawakal</span
         >
     </div>
     <span
